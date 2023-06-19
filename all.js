8a800620f3cc15e2badf78ef35b3880aa7e5617f
bucky0112 <bucky0112@gmail.com>
2023-06-19 16:13:20 +0800

    Refactor code to handle localStorage and improve event handling

--- all.js
+++ all.js
@@ -1,79 +1,69 @@
-const inputText = document.querySelector(".input");
-const add = document.querySelector(".add");
-const list = document.querySelector(".list");
-const deleteBtn = document.querySelector("ul");
-const listNum = document.querySelector(".listNum");
-const clean = document.querySelector(".cleanBTN");
+const inputText = document.querySelector('.input')
+const add = document.querySelector('.add')
+const list = document.querySelector('.list')
+const deleteBtn = document.querySelector('ul')
+const listNum = document.querySelector('.listNum')
+const clean = document.querySelector('.cleanBTN')
 
-let listData = []; // 原本使用 const，改用 let
-add.addEventListener("click", addList);
-clean.addEventListener("click", clearAll);
-deleteBtn.addEventListener("click", removeList);
-deleteBtn.addEventListener("click", status);
+// let listData = []
+let listData = JSON.parse(localStorage.getItem('todoList')) || []
+add.addEventListener('click', addList)
+clean.addEventListener('click', clearAll)
+deleteBtn.addEventListener('click', handleListEvent)
 
 function addList() {
-  const value = inputText.value.trim();
-  // console.log(value);
+  const value = inputText.value.trim()
   if (!value) {
-    return;
-  } else {
-    listData.push({
-      text: value,
-      status: false,
-      id: listData.length
-    });
+    return
   }
-  // console.log(listData);
-  render();
-  inputText.value = "";
+  listData.push({
+    text: value,
+    status: false,
+    id: listData.length
+  })
+  render()
+  inputText.value = ''
 }
 
 function render() {
-  let str = "";
+  let str = ''
   listData.forEach((item, i) => {
     str += `
             <li>
-                <label class='checkbox ${item.status ? "done" : ""}'>
+                <label class='checkbox ${item.status ? 'done' : ''}'>
                     <input type='checkbox' class='status' ${
-                    item.status ? "checked" : ""
+                      item.status ? 'checked' : ''
                     } data-num=${i}>
                     ${item.text}
                 </label>
                 <a href="" class='delete' data-num='${i}'>刪除</a>
             </li>
-        `;
-  });
-  list.innerHTML = str;
+        `
+  })
+  list.innerHTML = str
   listNum.innerHTML = `目前有 ${listData.length} 筆任務`
+
+  localStorage.setItem('todoList', JSON.stringify(listData))
 }
 
-function clearAll(e) { // 忘記補上 e 的參數
-    e.preventDefault();
-    listData = [];
-    render()
+function clearAll(e) {
+  e.preventDefault()
+  listData = []
+  render()
 }
 
-function removeList(e) {
-  e.preventDefault();
-  // console.log(e.target.nodeName);
-  if (e.target.nodeName === "A") {
-    listData.splice(e.target.dataset.num, 1);
-  }
-  render();
-}
-
-function status(e) {
-  // console.log(e.target.nodeName);
-  if (e.target.nodeName === "INPUT") {
+function handleListEvent(e) {
+  e.preventDefault()
+  if (e.target.nodeName === 'A') {
+    listData = listData.filter((item, index) => index != e.target.dataset.num)
+  } else if (e.target.nodeName === 'INPUT') {
     listData.forEach((item) => {
       if (e.target.dataset.num == item.id) {
-        if (item.status) {
-          item.status = false;
-        } else {
-          item.status = true;
-        }
+        item.status = !item.status
       }
-    });
-    render();
+    })
   }
+  render()
 }
+
+render()  // 在取得資料後馬上更新畫面
